033bdfb55d81cb7b485b9051b64c7633a871de46
mmaks17 <mmaks17@gmail.com>
2018-04-27 10:19:49 +0300

    Update newWebServer.sh

--- newWebServer.sh
+++ newWebServer.sh
@@ -158,7 +158,7 @@ read D
 echo $D
 
 
-useradd -g wheel $S /home/$S
+useradd -G wheel $S  
 usermod -a -G  apache $S
 echo "$S:$S" | chpasswd
 
@@ -170,10 +170,10 @@ chmod -R 775 /home/$S/www
 
 
 
-echo "CREATE USER '$S'@'%' IDENTIFIED BY '***';" |  mysql -uroot -pPASSWORD
-echo "GRANT USAGE ON *.* TO '$S'@'%' IDENTIFIED BY '***' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;"|  mysql -uroot -pPASSWORD
-echo "CREATE DATABASE IF NOT EXISTS $S;"  |  mysql -uroot -pPASSWORD
-echo "GRANT ALL PRIVILEGES ON $S.* TO '$S'@'%';"  |  mysql -uroot -pPASSWORD
+echo "CREATE USER '$S'@'%' IDENTIFIED BY '$S';" |  mysql -uroot -p11
+echo "GRANT USAGE ON *.* TO '$S'@'%' IDENTIFIED BY '$S' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;"|  mysql -uroot -p11
+echo "CREATE DATABASE IF NOT EXISTS $S;"  |  mysql -uroot -p11
+echo "GRANT ALL PRIVILEGES ON $S.* TO '$S'@'%';"  |  mysql -uroot -p11
 
 
 cat <<EOT>>  /etc/httpd/conf.d/$S.$D.conf
